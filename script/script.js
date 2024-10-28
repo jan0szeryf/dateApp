53393c9f1ab6e3d75484113b33ed0441b3fdc44d
Janek <134413249+jan0szeryf@users.noreply.github.com>
2024-10-28 21:28:30 +0100

    Need to move the project to NodeJS to have the ability to render JSON files

--- script/script.js
+++ script/script.js
@@ -9,6 +9,8 @@ const noButton = () => {
     document.getElementById("yesButton").style.fontSize = (20 + noButton_i*20 + "px");
 
     let noButton = document.getElementById("noButton");
+    let susEmojiImg = document.getElementById("susEmoji");
+    let susSound = new Audio('./media/sus.mp3');
 
     noButton.style.position = "absolute";
     noButton.style.top = (randomInt(0, (window.innerHeight-153)) + "px");
@@ -22,4 +24,32 @@ const noButton = () => {
         if(noButton_i % 4 == 2)
             noButton.innerHTML = "czm nie chcesz?";
     }
+
+    susEmojiImg.style.top = (randomInt(0, (window.innerHeight-153)) + "px");
+    susEmojiImg.style.left = (randomInt(0, (window.innerWidth-140)) + "px");
+    susEmojiImg.style.transform = "rotate(" + randomInt(-30, 30) + "deg)";
+    susEmojiImg.style.width = 5 + noButton_i + "vw";
+    susEmojiImg.style.height = 5 + noButton_i + "vw";
+    susEmojiImg.style.opacity = 1;
+
+    setTimeout(function() {
+        susEmojiImg.style.opacity = 0;
+    }, 1000);
+
+    if(noButton_i < 10) {
+        susSound.volume = 0.1 * noButton_i;
+    }
+
+    console.log(susSound.volume);
+
+    susSound.play();
+}
+
+const selectOption = (val) => {
+    let entry = {
+        date: new Date(),
+        choice: val
+    };
+
+    const stringEntry = JSON.stringify(entry);
 }
